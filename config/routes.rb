4efff015d3e27b343f548053d43c5d65f576b392
Vera Ho <vh71886@gmail.com>
2022-07-27 10:32:32 -0400

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'search/index'
+  # get 'search/index'
+  get '/search' => 'search#index', :as => 'search'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
