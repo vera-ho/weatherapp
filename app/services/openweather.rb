d0dd9a61481e6cc5932d1b1302c7d6a9fb743497
Vera Ho <vh71886@gmail.com>
2022-07-27 10:57:57 -0400

    Add parens

--- app/services/openweather.rb
+++ app/services/openweather.rb
@@ -3,7 +3,7 @@ module Openweather
       def self.by_location(location)
         # puts "key is:"
         # puts ENV['API_KEY']
-        Faraday.get 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY']
+        Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
       end
     end
   end
