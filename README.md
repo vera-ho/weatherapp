91a773cc9677d162abc3dcf5e5208c49e6b9f713
Vera Ho <vh71886@gmail.com>
2022-07-28 08:49:14 -0400

    Got one hotel API to work

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # README
 This project is to test how Rails can make API calls
 * https://dev.to/juzqrios/how-to-make-api-calls-in-rails-26l9
-* 
+* https://rapidapi.com/blog/how-to-use-an-api-with-ruby/
--- app/services/openweather.rb
+++ app/services/openweather.rb
@@ -1,12 +1,19 @@
 module Openweather
     class Search
-      def self.by_location(location)
-        # puts "key is:"
-        # puts ENV['API_KEY']
-        # Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
-        # Excon.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
-        HTTParty.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
+        def self.by_location(location)
+            puts "key is:"
+            puts ENV['API_KEY']
+            # Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
+            # Excon.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
 
-      end
+            url = 'https://sandbox.impala.travel/v1/hotels'
+            HTTParty.get(
+                url,
+                headers: {
+                    'x-api-key': ENV['API_KEY'],
+                    "Content-Type": "application/json",
+                }
+            )
+        end
     end
-  end
+end
