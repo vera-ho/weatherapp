09b9faa37dbc25d30582dfc7a77e61196ab9c684
Vera Ho <vh71886@gmail.com>
2022-07-27 23:08:34 -0400

    Verified call also works with Excon gem

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README
 This project is to test how Rails can make API calls
 * https://dev.to/juzqrios/how-to-make-api-calls-in-rails-26l9
+* 
--- app/services/openweather.rb
+++ app/services/openweather.rb
@@ -3,7 +3,8 @@ module Openweather
       def self.by_location(location)
         # puts "key is:"
         # puts ENV['API_KEY']
-        Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
+        # Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
+        Excon.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'])
       end
     end
   end
